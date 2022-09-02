ea1a23e4fe04ab6ea33d6dfebe111feb20e4fa6d
naihe <239144498@qq.com>
2022-09-02 21:42:20 +0800

    docker 自动构建发布

--- app/main.py
+++ app/main.py
@@ -11,3 +11,6 @@ if __name__ == '__main__':
     Thread(target=everyday, args=(2,)).start()
     print(localhost)
     uvicorn.run(app, host="0.0.0.0", port=PORT, log_level="info")   # reload=True, debug=True
+
+# https://github.com/239144498/Streaming-Media-Server-Pro/workflows/docker-image/badge.svg?branch=main
+# https://github.com/<OWNER>/<REPOSITORY>/workflows/<WORKFLOW_NAME>/badge.svg?branch=<branch-name>
